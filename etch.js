e3ccca141a9f25d63fe6c02dde7f95c686e77df5
Young Lee <lee.yghoon@gmail.com>
2023-03-08 00:25:43 -0500

    Center gridbox and begin javascript for hover function

--- etch.js
+++ etch.js
@@ -6,3 +6,11 @@ for (let i = 0; i < 16; i++) {
   multiply.setAttribute("id", "boxes");
   grid.appendChild(multiply);
 }
+
+document.getElementById("boxes").addEventListener("mouseover", function () {
+  document.getElementById("boxes").style.backgroundColor = "green";
+});
+
+document.getElementById("boxes").addEventListener("mouseout", function () {
+  document.getElementById("boxes").style.backgroundColor = "red";
+});
