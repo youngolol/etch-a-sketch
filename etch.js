a1634d0a41b92658605727681f3043bc71f505c5
Young Lee <lee.yghoon@gmail.com>
2023-03-11 02:00:14 -0500

    Add pseudocode to write steps for dynamic grid change and add slider bar

--- etch.js
+++ etch.js
@@ -7,12 +7,14 @@ for (let i = 0; i < 256; i++) {
   grid.appendChild(multiply);
 }
 
+// this method will create event listeners for every div
 // document.querySelectorAll(".items").forEach((item) => {
 //   item.addEventListener("mouseover", (event) => {
 //     item.style.backgroundColor = "black";
 //   });
 // });
 
+// this method ultimately creates an event listener for whatever div is hovered over
 document.getElementById("box").addEventListener("mouseover", (event) => {
   const selectedId = event.target.id;
   console.log(selectedId);
@@ -20,3 +22,18 @@ document.getElementById("box").addEventListener("mouseover", (event) => {
   console.log(selectedDiv);
   selectedDiv.style.backgroundColor = "black";
 });
+
+// creating slider bar to enact the following function: change grid size relative to the slider value within the dimensions of the original bo in 16x16
+
+// var slider = document.getElementById("myRange");
+// var output = document.getElementById("demo");
+// output.innerHTML = slider.value;
+
+// slider.oninput = function() {
+//   output.innerHTML = this.value;
+// }
+
+// get value of element
+// input eventlistener via DOM manip for user input of slider change
+// square value insert into variable
+// adjust for-loop to account for change to grid size and spit out appropriate grid
